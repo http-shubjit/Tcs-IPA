a23dd446982d9da5feb75c81c327640f6178a1b3
Shubhajit <shubjitgithub@gmail.com>
2024-10-18 18:31:12 +0530

    Address

--- IPA4.java
+++ IPA4.java
@@ -1,296 +1,296 @@
-// Create a class IPA4 with the below attributes.
+// // Create a class IPA4 with the below attributes.
 
-//  id-int 
+// //  id-int 
 
-//  name -String 
+// //  name -String 
 
-//  contactNo-int
+// //  contactNo-int
 
-//  address-String 
+// //  address-String 
 
-//  pinCode-int
-
-
-
-// Write the getters and setters and parametrized constructor in the above 
-
-// mentioned sequence as required.
+// //  pinCode-int
 
 
 
-// Create the class Solution with the main method.
+// // Write the getters and setters and parametrized constructor in the above 
 
-// Implement the two static methods:
-
-//  1.findIPA4WithMaximumPincode 
-
-//  2.searchIPA4ByAddress
+// // mentioned sequence as required.
 
 
 
-// findIPA4WithMaximumPincode method:
+// // Create the class Solution with the main method.
 
-//    Create the Static method in the Solution Class.
+// // Implement the two static methods:
 
-//  This method will take array of the IPA4 objects and return the 
+// //  1.findIPA4WithMaximumPincode 
 
-// IPA4 object having maximum pincode if found else return null if not 
-
-// found.
-
-//    for this method ,main method will print IPA4 object with maximum
-
-// pincode if the returned value is not null.if the returned value is null ,
-
-
-// then the main method will print "No IPA4 found with mentioned attribute".
+// //  2.searchIPA4ByAddress
 
 
 
+// // findIPA4WithMaximumPincode method:
+
+// //    Create the Static method in the Solution Class.
+
+// //  This method will take array of the IPA4 objects and return the 
+
+// // IPA4 object having maximum pincode if found else return null if not 
+
+// // found.
+
+// //    for this method ,main method will print IPA4 object with maximum
+
+// // pincode if the returned value is not null.if the returned value is null ,
 
 
-// searchIPA4ByAddress method:
-
-//    Create the Static method in the Solution Class.
-
-// This method will take array of IPA4 objects as input and address as 
-
-// input and return IPA4 object having the mentioned address if found 
-
-// else return null if not found.
-
-//    for this method main method will print IPA4 object details as it is ,
-
-//  if the returned value is not null.if the returned value is null then ,
+// // then the main method will print "No IPA4 found with mentioned attribute".
 
 
-// main method will print "No IPA4 found with mentioned attribute".
+
+
+
+// // searchIPA4ByAddress method:
+
+// //    Create the Static method in the Solution Class.
+
+// // This method will take array of IPA4 objects as input and address as 
+
+// // input and return IPA4 object having the mentioned address if found 
+
+// // else return null if not found.
+
+// //    for this method main method will print IPA4 object details as it is ,
+
+// //  if the returned value is not null.if the returned value is null then ,
+
+
+// // main method will print "No IPA4 found with mentioned attribute".
 
 
  
 
-// ***************************************************************************
+// // ***************************************************************************
 
-// input 1:
+// // input 1:
 
-// 4
-// 109 
-// ACT
-// 2500256
-// mumbai
-// 695001
-// 107
-// MCE
-// 2500254
-// malapuram
-// 612354
-// 113
-// CTE
-// 2500252
-// chennai
-// 623145
-// 102
-// SCT
-// 2500255
-// AP
-// 523641
-// AP
-
-
-
-// OUTPUT1:
-
-// id-109
-
-// name-ACT
-
-// contactNo-2500256
-
-// address-mumbai
-
-// pincode-695001
-
-// id-102
-
-// name-SCT
-
-// contactNo-2500255
-
-// address-AP
-
-// pincode-523641
+// // 4
+// // 109 
+// // ACT
+// // 2500256
+// // mumbai
+// // 695001
+// // 107
+// // MCE
+// // 2500254
+// // malapuram
+// // 612354
+// // 113
+// // CTE
+// // 2500252
+// // chennai
+// // 623145
+// // 102
+// // SCT
+// // 2500255
+// // AP
+// // 523641
+// // AP
 
 
+
+// // OUTPUT1:
+
+// // id-109
+
+// // name-ACT
+
+// // contactNo-2500256
+
+// // address-mumbai
+
+// // pincode-695001
+
+// // id-102
+
+// // name-SCT
+
+// // contactNo-2500255
+
+// // address-AP
+
+// // pincode-523641
 
 
 
 
 
-// INPUT2:
-
-// 4
-// 111
-// MJT
-// 2500251
-// Calicut
-// 401235
-// 105
-// MET
-// 2500256
-// kochi
-// 668745
-// 115
-// IIT
-// 2500262
-// banglore
-// 569874
-// 110
-// ACT
-// 2500263
-// delhi
-// 687945
-// delhi
 
 
-// output2:
+// // INPUT2:
 
-// id-110
+// // 4
+// // 111
+// // MJT
+// // 2500251
+// // Calicut
+// // 401235
+// // 105
+// // MET
+// // 2500256
+// // kochi
+// // 668745
+// // 115
+// // IIT
+// // 2500262
+// // banglore
+// // 569874
+// // 110
+// // ACT
+// // 2500263
+// // delhi
+// // 687945
+// // delhi
 
-// name-ACT
 
-// contactNo-2500263
+// // output2:
 
-// address-delhi
+// // id-110
 
-// pincode-687945
+// // name-ACT
 
-// id-110
+// // contactNo-2500263
 
-// name-ACT
+// // address-delhi
 
-// contactNo-2500263
+// // pincode-687945
 
-// address-delhi
+// // id-110
 
-// pincode-687945
+// // name-ACT
 
-import java.util.Scanner;
+// // contactNo-2500263
 
-class Solution {
-    public static void main(String[] args) {
-Scanner sc=new Scanner(System.in);
-        IPA4[] IPA4s = new IPA4[5];
-        for (int i = 0; i < IPA4s.length; i++) {
-             int id=sc.nextInt();
-             String name = sc.next();
-             int contactNo=sc.nextInt();
-             String address=sc.next();
-             int pinCode = sc.nextInt();
-             IPA4s[i] = new IPA4(id, name, contactNo, address, pinCode);
-        }
+// // address-delhi
 
-        IPA4 pin = findIPA4WithMaximumPincode(IPA4s);
-       System.out.println(pin);
- String address = sc.nextLine();
- IPA4 addr = searchIPA4ByAddress(IPA4s, address);
- System.out.println(addr);
- sc.close();
-    }
+// // pincode-687945
 
-    private static IPA4 searchIPA4ByAddress(IPA4[] IPA4s, String address) {
+// import java.util.Scanner;
+
+// class Solution {
+//     public static void main(String[] args) {
+// Scanner sc=new Scanner(System.in);
+//         IPA4[] IPA4s = new IPA4[5];
+//         for (int i = 0; i < IPA4s.length; i++) {
+//              int id=sc.nextInt();
+//              String name = sc.next();
+//              int contactNo=sc.nextInt();
+//              String address=sc.next();
+//              int pinCode = sc.nextInt();
+//              IPA4s[i] = new IPA4(id, name, contactNo, address, pinCode);
+//         }
+
+//         IPA4 pin = findIPA4WithMaximumPincode(IPA4s);
+//        System.out.println(pin);
+//  String address = sc.nextLine();
+//  IPA4 addr = searchIPA4ByAddress(IPA4s, address);
+//  System.out.println(addr);
+//  sc.close();
+//     }
+
+//     private static IPA4 searchIPA4ByAddress(IPA4[] IPA4s, String address) {
        
-        for (int i = 0; i < IPA4s.length; i++) {
-            if (IPA4s[i].getAddress().equalsIgnoreCase(address))
-                return IPA4s[i];
-        }
-        return null;
+//         for (int i = 0; i < IPA4s.length; i++) {
+//             if (IPA4s[i].getAddress().equalsIgnoreCase(address))
+//                 return IPA4s[i];
+//         }
+//         return null;
 
-    }
+//     }
 
-    private static IPA4 findIPA4WithMaximumPincode(IPA4[] IPA4s) {
-        int max = 0;        
-        for (int i = 0; i < IPA4s.length; i++) {
-            if (IPA4s[i].getPinCode() > max) {
-                max = IPA4s[i].getPinCode();
-            }
-        }
-        for (int i = 0; i < IPA4s.length; i++) {
-            if (IPA4s[i].getPinCode() == max) {
-                return IPA4s[i];
-            }
-        }
-        return null;
+//     private static IPA4 findIPA4WithMaximumPincode(IPA4[] IPA4s) {
+//         int max = 0;        
+//         for (int i = 0; i < IPA4s.length; i++) {
+//             if (IPA4s[i].getPinCode() > max) {
+//                 max = IPA4s[i].getPinCode();
+//             }
+//         }
+//         for (int i = 0; i < IPA4s.length; i++) {
+//             if (IPA4s[i].getPinCode() == max) {
+//                 return IPA4s[i];
+//             }
+//         }
+//         return null;
     
-    }
-}
+//     }
+// }
 
- public class IPA4 {
-    // Attributes
-    private int id;
-    private String name;
-    private int contactNo;
-    private String address;
-    private int pinCode;
+//  public class IPA4 {
+//     // Attributes
+//     private int id;
+//     private String name;
+//     private int contactNo;
+//     private String address;
+//     private int pinCode;
 
-    // Constructor
-    public IPA4(int id, String name, int contactNo, String address, int pinCode) {
-        this.id = id;
-        this.name = name;
-        this.contactNo = contactNo;
-        this.address = address;
-        this.pinCode = pinCode;
-    }
+//     // Constructor
+//     public IPA4(int id, String name, int contactNo, String address, int pinCode) {
+//         this.id = id;
+//         this.name = name;
+//         this.contactNo = contactNo;
+//         this.address = address;
+//         this.pinCode = pinCode;
+//     }
 
-    // Getter and Setter methods
-    public int getId() {
-        return id;
-    }
+//     // Getter and Setter methods
+//     public int getId() {
+//         return id;
+//     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+//     public void setId(int id) {
+//         this.id = id;
+//     }
 
-    public String getName() {
-        return name;
-    }
+//     public String getName() {
+//         return name;
+//     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+//     public void setName(String name) {
+//         this.name = name;
+//     }
 
-    public int getContactNo() {
-        return contactNo;
-    }
+//     public int getContactNo() {
+//         return contactNo;
+//     }
 
-    public void setContactNo(int contactNo) {
-        this.contactNo = contactNo;
-    }
+//     public void setContactNo(int contactNo) {
+//         this.contactNo = contactNo;
+//     }
 
-    public String getAddress() {
-        return address;
-    }
+//     public String getAddress() {
+//         return address;
+//     }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+//     public void setAddress(String address) {
+//         this.address = address;
+//     }
 
-    public int getPinCode() {
-        return pinCode;
-    }
+//     public int getPinCode() {
+//         return pinCode;
+//     }
 
-    public void setPinCode(int pinCode) {
-        this.pinCode = pinCode;
-    }
+//     public void setPinCode(int pinCode) {
+//         this.pinCode = pinCode;
+//     }
 
-    // toString method for easy representation
-    @Override
-    public String toString() {
-        return "IPA4{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", contactNo=" + contactNo +
-                ", address='" + address + '\'' +
-                ", pinCode=" + pinCode +
-                '}';
-    }
-}
+//     // toString method for easy representation
+//     @Override
+//     public String toString() {
+//         return "IPA4{" +
+//                 "id=" + id +
+//                 ", name='" + name + '\'' +
+//                 ", contactNo=" + contactNo +
+//                 ", address='" + address + '\'' +
+//                 ", pinCode=" + pinCode +
+//                 '}';
+//     }
+// }
