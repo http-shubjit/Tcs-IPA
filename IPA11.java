0c94d53c920eeb3a9d634e1f33a7053c8466b669
Shubhajit <shubjitgithub@gmail.com>
2024-11-02 10:40:50 +0530

    CountWord

--- IPA11.java
+++ IPA11.java
@@ -1,3 +1,28 @@
+// /* Input : Home, Sweet Home
+//            Home
+//  * Output : Number of that word present in the sentence : 2
+//  * 
+//  * Process
+//  * ----------
+//  * The word Home present in the sentence 2 times
+//  */
+
+// import java.util.*;
+
+// public class Find_CountWord {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         String str = sc.nextLine();
+//         String[] arr = str.split("\\W+");
+//         String s = sc.nextLine();
+//      int count=0;
+//         for (int i = 0; i < arr.length; i++) {
+//             if(arr[i].equals(s))
+           //count++
+//         }
+//         System.out.println("Number of that word present in the Sentence : " + count);
+//     }
+// }
 // Create a class AutonomousCar with the below attributes:
 
 // carld – int
