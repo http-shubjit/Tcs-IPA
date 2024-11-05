89ee627f49675bfd9bc8528587dc39eab223b3d9
Shubhajit <shubjitgithub@gmail.com>
2024-11-05 18:21:52 +0530

    Fibonacci Without recurssion

--- IPA_Simple.java
+++ IPA_Simple.java
@@ -62,3 +62,50 @@
 //         System.out.println(Arrays.toString(resarr));
 //     }
 // }
+
+//3:
+/*
+ * Write a Java program that takes a user input integer n and prints the
+ * Fibonacci series up to n.
+ * 
+ * The Fibonacci series is a series of numbers in which each number is the sum
+ * of the two preceding numbers.
+ * The first two numbers in the series are 0 and 1. For example, the first 10
+ * numbers in the Fibonacci series are:
+ * 0, 1, 1, 2, 3, 5, 8, 13, 21, 34.
+ * 
+ * The program should use a for loop to generate the series.
+ * 
+ * Sample Input:
+ * 20
+ * 
+ * Sample Output:
+ * 0 1 1 2 3 5 8 13
+ * 
+ * Explanation:
+ * The Fibonacci series up to 20 is: 0, 1, 1, 2, 3, 5, 8, 13. Therefore, the
+ * output is "0 1 1 2 3 5 8 13".
+ */
+
+// import java.util.Scanner;
+
+// class Solution {
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         System.out.println(0);
+//         System.out.println(1);
+//         int count = 2;
+//         int prev=0;
+//         int next=1;
+//         while (count <= n) {
+//             int fib = prev + next;
+//             prev = next;
+//             next = fib;
+//             System.out.println(fib);
+//             count++;
+//         }
+
+//     }
+// }
+
